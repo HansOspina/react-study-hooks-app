c1936faf17da8cf73adbd93803d6453337467b63
Hans Ospina <hansospina@gmail.com>
2020-07-21 20:50:33 -0500

    useState

---
+++ src/components/01-useState/CounterApp.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import './counter.css'
+
+const CounterApp = () => {
+
+  const [counters, setCounters] = useState({counter1:10, counter2:20, counter3:30, counter4:40});
+
+  // de-structure
+  const {counter1, counter2}  = counters
+
+  // spread and effect by function
+  const increment = ()=> setCounters(prev  => ({...prev,counter1:(counter1+1)}));
+
+  return (
+    <>
+     <h1>Counter1 {counter1}</h1>
+      <h1>Counter2 {counter2}</h1>
+      <hr/>
+      <button
+        className="btn btn-primary"
+        onClick={increment}
+      >+1</button>
+
+    </>
+  );
+};
+
+export default CounterApp;
